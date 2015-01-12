91f2a81e8a8a896d5ecc254d80f44dd516237c3a
ryzy <marcin@ryzycki.com>
2015-01-12 12:30:15 +0000

    !!! fastcgi-cache.conf renamed so it's not included by default
    In most cases the default fastcgi cache is not used. Therefore it makes sense to not to load it by default. It's easy to switch it on though, by simply renaming this file back to .conf. Or copy/create it in sub-images with custom configuration.

--- container-files/config/init/10-nginx-cache-dirs.sh
+++ container-files/config/init/10-nginx-cache-dirs.sh
@@ -3,6 +3,7 @@
 #
 # This script will be placed in /config/init/ and run when container starts.
 # It creates (if they're not exist yet) necessary Nginx directories
+# @see /etc/nginx/addon.d/fastcgi-cache.example
 #
 
 set -e
