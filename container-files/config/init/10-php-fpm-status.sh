f14694229b2ae0c20cb4ae93bc1b1b4e5d0e8009
Marcin Ryzycki <marcin@ryzycki.com>
2015-06-23 12:26:00 +0100

    [TASK] Log message for status page corrected
    Follow up for 48be87442cb14fb534ead2a61bcfe0cb8b758256 change.

--- container-files/config/init/10-php-fpm-status.sh
+++ container-files/config/init/10-php-fpm-status.sh
@@ -3,4 +3,4 @@
 # Configure access IP address for PHP-FPM satus page under /fpm_status
 #
 sed -i "s|allow 127.0.0.1|allow ${STATUS_PAGE_ALLOWED_IP}|g" /etc/nginx/conf.d/fpm-status.conf
-echo "Nginx status page: allowed address set to: $STATUS_PAGE_ALLOWED_IP"; echo
+echo "PHP-FPM status page: allowed address set to $STATUS_PAGE_ALLOWED_IP."
