6ccf58d87ac62af602840ed1ac09707af4d2decf
ryzy <marcin@ryzycki.com>
2015-12-19 00:27:02 +0000

    [TASK] Set PHP temp dir to /data/tmp/php
    Switching all temporary directories (upload, session) to /data/tmp/php
    so when more container use the same /data/ volume, this data can be
    accessible.

--- container-files/config/init/21-php-fpm-dirs.sh
+++ container-files/config/init/21-php-fpm-dirs.sh
@@ -8,6 +8,8 @@
 
 set -e
 
-mkdir -p /run/user/nginx-cache
-mkdir -p /run/user/nginx-cache-tmp
-chown -R www:www /run/user/nginx-cache*
+mkdir -p /data/tmp/php/uploads
+mkdir -p /data/tmp/php/sessions
+mkdir -p /data/tmp/nginx/fastcgi_cache
+mkdir -p /data/tmp/nginx/fastcgi_cache_tmp
+chown -R www:www /data/tmp/php /data/tmp/nginx/fastcgi_cache*
