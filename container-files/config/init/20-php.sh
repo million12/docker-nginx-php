ea7b8ed4f7f22170a498ff3acc32862dbb45b03b
ryzy <marcin@ryzycki.com>
2015-12-19 23:05:02 +0000

    FIX Bash exiting due to unbound variable

--- container-files/config/init/20-php.sh
+++ container-files/config/init/20-php.sh
@@ -8,7 +8,6 @@
 # in non-standard PATHs, we need to configure it, which does the following script
 # See Dockerfile - the following script is symlinked to /opt/remi/php70/enable.
 #
-# Note: wrap in set +-e as there are some unbound variables and our scripts are strict about it.
-set +e
+# Note: define MANPATH so bash doesn't complain about unbound MANPATH variable from there
+export MANPATH=""
 source /etc/profile.d/php70-paths.sh
-set -e
